900d60f744cbe66b4d79da43ab82598b03a6d04d
JonasKennedy <93797170+jkgprep@users.noreply.github.com>
2021-11-17 20:36:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+#Jonas Kennedy, 11/17/21, program which determinds if a triangle is equillateral, isoceles or, scalene
+side1 = int(input("What is the length of the 1st side? \n"))
+side2 = int(input("what is the length of the 2nd side? \n"))
+side3 = int(input("what is the length of the 3rd side? \n"))
